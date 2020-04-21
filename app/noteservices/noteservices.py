705dd936d6370483aab0ad64351c36be0ddbae0d
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-04-22 02:27:31 +0530

    [Divya]refactor:in noteservicses imported the db_session and Note from database,modlesthen implemented the functional logic to add and commit data into table

--- app/noteservices/noteservices.py
+++ app/noteservices/noteservices.py
@@ -1,6 +1,8 @@
 # http_service.py
 import json
 from nameko.rpc import rpc
+from database import db_session
+from models import Note
 
 config = {'AMQP_URI':'amqp://guest:guest@localhost/'}
 
@@ -9,14 +11,21 @@ class HttpNoteService:
     
     @rpc
     def create_note(self, request,title,text,archive,color):
+        note = Note(title,text,archive,color)
+        db_session.add(note)
+        db_session.commit()
+        db_session.remove()
         return "successfully created the note"
 
     @rpc     
     def edit_note(self,id,title,text,archive,color):
+        
         return "successfully updated the note"
+        
     
     @rpc
     def delete_note(self,id):
+        
         return "successfully deleted the note"
         
         
