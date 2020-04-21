d150b98ab53f1848b8d0af1254aab19bcbece0ce
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-04-22 01:12:13 +0530

    [Divya]add:in gatewaysservices initialized rabbitmq connection in the config

--- app/gatewayservices/gatewayservices.py
+++ app/gatewayservices/gatewayservices.py
@@ -2,9 +2,11 @@
 import json 
 from nameko.web.handlers import http
 
-class HttpService:
+config = {'AMQP_URI':'amqp://guest:guest@localhost/'}
+
+class HttpRegistrationService:
     name = "gatewayservices"
-        
+    
     @http('GET', '/get')
     def get_method(self, request):
         return "welcome to Services"
