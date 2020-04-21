4c34cc451b69e118863335cdb56cb51b4f474d38
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-04-22 02:22:47 +0530

    [Divya]refactor:using sqlalchemy packages created the table then mapping notemodel to table note model and from database imported the db_session and metadata to writequery

--- app/noteservices/models.py
+++ app/noteservices/models.py
@@ -1,5 +1,10 @@
+from sqlalchemy import Table, Column, Integer, String, Boolean,ForeignKey,ForeignKeyConstraint
+from sqlalchemy.orm import mapper,relationship,backref
+from database import metadata, db_session 
+
 class Note(object):
-   
+    query = db_session.query_property()
+
     def __init__(self, title=None, text=None, archive=None, color=None,userid=None):
         self.title = title
         self.text = text
@@ -10,3 +15,14 @@ class Note(object):
         return '<Note %r>' % (self.title)
 
 
+notes = Table('notes', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('title', String),
+    Column('text', String),
+    Column('color', String),
+    Column('archive',Boolean,default=False,nullable=False),
+    Column('userid',Integer,ForeignKey("users.id"),nullable=False),
+    
+)
+
+mapper(Note,notes)
