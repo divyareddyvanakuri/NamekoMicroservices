bbedf4c555ebe00d2b0af9e5eacec46641d00550
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-04-22 01:50:46 +0530

    [Divya]refactor:using sqlalchemy created the user model table and query for db_session

--- app/userservices/models.py
+++ app/userservices/models.py
@@ -1,5 +1,10 @@
+from sqlalchemy import Table, Column, Integer, String, Boolean,ForeignKey,ForeignKeyConstraint
+from sqlalchemy.orm import mapper,relationship,backref
+from database import metadata, db_session 
+
 class User(object):
-    
+    query = db_session.query_property()c
+
     def __init__(self, username=None, email=None, password=None):
         self.username = username
         self.email = email
@@ -7,3 +12,15 @@ class User(object):
 
     def __repr__(self):
         return '<User %r>' % (self.username)
+        
+    
+
+users = Table('users', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('username', String(50), unique=True),
+    Column('email', String(120), unique=True),
+    Column('password', String),
+    # relationship('Note', backref='person', lazy=True)
+
+)
+
