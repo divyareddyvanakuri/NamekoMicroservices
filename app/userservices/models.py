5227b1ff8eab57cae4dce11cb7d25becb340d3ce
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-04-22 01:41:22 +0530

    [Divya]refactor:in userservices created the User model in models.py file and initialized atrributes

---
+++ app/userservices/models.py
@@ -0,0 +1,11 @@
+class User(object):
+    
+    def __init__(self, username=None, email=None, password=None):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+    
+
