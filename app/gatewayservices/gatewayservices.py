7ce0789830b0ba83bca3a0108aa158e8d726b7b7
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-04-22 01:14:48 +0530

    [Divya]refactor:in gateservices created post method for user registration

--- app/gatewayservices/gatewayservices.py
+++ app/gatewayservices/gatewayservices.py
@@ -2,13 +2,23 @@
 import json 
 from nameko.web.handlers import http
 
+
 config = {'AMQP_URI':'amqp://guest:guest@localhost/'}
 
 class HttpRegistrationService:
     name = "gatewayservices"
     
+   
     @http('GET', '/get')
     def get_method(self, request):
         return "welcome to Services"
+
+    @http('POST', '/register')
+    def register(self, request):
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirmpassword = request.form['confirmpassword']
+       
 
     
